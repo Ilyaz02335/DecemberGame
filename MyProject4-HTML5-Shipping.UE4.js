e656e956a0de2244af7751ebb38e3afff8e26b1e
Ilyaz02335 <147247680+Ilyaz02335@users.noreply.github.com>
2023-12-26 21:06:08 +0700

    Update MyProject4-HTML5-Shipping.UE4.js

--- MyProject4-HTML5-Shipping.UE4.js
+++ MyProject4-HTML5-Shipping.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('MyProject4-HTML5-Shipping.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/Ilyaz02335/DecemberGame/d453e76609b517be437a039016a143e9bcd9e4fa/MyProject4-HTML5-Shipping.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
